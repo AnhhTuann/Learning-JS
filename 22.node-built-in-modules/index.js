a19231f0476002d36995c27e620ca87d69f59a3c
AnhhTuann <tuannguyen25101999@gmail.com>
2020-05-04 10:26:12 +0700

    deleted all

--- 22.node-built-in-modules/index.js
+++
@@ -1,8 +0,0 @@
-//Node built-in modules
-var fs = require('fs');
-
-var text = fs.readFileSync('./song.txt', {encoding:'utf8'});
-
-console.log(text);
-
-fs.writeFileSync('.song_2.txt','I\'m Tuan');
--- 5.Function/function.js
+++
@@ -1,6 +0,0 @@
-//5.Function 
-function calculateTriangSquare (a,h) {
-    return a*h/2;
-}
-console.log (calculateTriangSquare(10,5));
-console.log('=======================================================');
